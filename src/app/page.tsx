6abaa6a25a5f0cae8bbbe29b184aa29a11c8d0df
Miłosz <milosz.devmail@gmail.com>
2023-09-28 23:46:44 +0200

    Add steps section of lading page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -82,6 +82,63 @@ export default function Home() {
           </div>
         </div>
       </div>
+      {/* Feature section */}
+      <div className="mx-auto mb-32 mt-32 max-w-5xl sm:mt-56">
+        <div className="mb-12 px-6 lg:px-8">
+          <div className="mx-auto max-w-2xl sm:text-center">
+            <h2 className="mt-2 font-bold text-4xl text-gray-900 sm:text-5xl">
+              Start chatting... actually in minutes
+            </h2>
+            <p className="mt-4 text-lg text-gray-600">
+              Have you ever tried talking to a document?
+              <br />
+              With <span className="text-primary font-bold">Quesdoc</span> it is
+              possible. And the experience is great!
+            </p>
+          </div>
+        </div>
+        {/* steps */}
+        <ol className="my-8 space-y-4 pt-8 md:flex md:space-x-12 md:space-y-0">
+          <li className="md:flex-1">
+            <div className="flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4">
+              <span className="text-sm font-medium text-primary">Step 1</span>
+              <span className="text-xl font-semibold">
+                Sign up for an account
+              </span>
+              <span className="mt-2 text-zinc-700">
+                Either starting out with a free plan or choose our{" "}
+                <Link href="/pricing" className="text-primary underline">
+                  pro plan.
+                </Link>
+              </span>
+            </div>
+          </li>
+          <li className="md:flex-1">
+            <div className="flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4">
+              <span className="text-sm font-medium text-primary">Step 2</span>
+              <span className="text-xl font-semibold">
+                Upload your PDF file
+              </span>
+              <span className="mt-2 text-zinc-700">
+                We{"'"}ll process the file and make it ready for you to chat
+                with.
+              </span>
+            </div>
+          </li>
+          <li className="md:flex-1">
+            <div className="flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4">
+              <span className="text-sm font-medium text-primary">Step 3</span>
+              <span className="text-xl font-semibold">
+                Start asking questions
+              </span>
+              <span className="mt-2 text-zinc-700">
+                It{"'"}s that simple! Try Quesdoc today - it really takes a
+                minute...
+              </span>
+            </div>
+          </li>
+        </ol>
+      </div>
     </>
   );
 }
