d9f9b476bd66eee1c957312f7ef398d5f8e83523
Miłosz <milosz.devmail@gmail.com>
2023-09-29 13:55:23 +0200

    Decrease margin top for images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -54,7 +54,7 @@ export default function Home() {
           {/* dashboard preview */}
           <div>
             <div className="mx-auto max-w-6xl px-6 lg:px-8">
-              <div className="mt-60 flow-root sm:mt-24">
+              <div className="mt-32 flow-root sm:mt-24">
                 <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
                   <Image
                     src="/dashboard-preview.jpg"
@@ -139,7 +139,7 @@ export default function Home() {
           </li>
         </ol>
         <div className="mx-auto max-w-6xl px-6 lg:px-8">
-          <div className="mt-60 flow-root sm:mt-24">
+          <div className="mt-32 flow-root sm:mt-24">
             <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
               <Image
                 src="/file-upload-preview.jpg"
