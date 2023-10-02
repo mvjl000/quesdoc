84b558dd60a3e5ef13a6af40aa6fec4db3252555
Miłosz <milosz.devmail@gmail.com>
2023-10-02 16:00:40 +0200

    Create basic pdf renderer top bar, install pdf package

--- src/components/PdfRenderer.tsx
+++ src/components/PdfRenderer.tsx
@@ -1,7 +1,9 @@
 export const PdfRenderer = () => {
   return (
-    <div>
-      <p>PDF renderer</p>
+    <div className="w-full bg-white rounded-md shadow flex flex-col items-center">
+      <div className="h-14 w-full border-b border-zinc-200 flex justify-between items-center px-2">
+        <div className="flex items-center gap-1.5">top bar</div>
+      </div>
     </div>
   );
 };
