266f8b146e587806158eb2d06524448fae1d898f
Miłosz <milosz.devmail@gmail.com>
2023-10-04 14:38:46 +0200

    Handle resizing of a PDF

--- src/components/PdfRenderer.tsx
+++ src/components/PdfRenderer.tsx
@@ -5,6 +5,7 @@ import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 import "react-pdf/dist/esm/Page/TextLayer.css";
 import { useToast } from "@/components/ui/use-toast";
+import { useResizeDetector } from "react-resize-detector";
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
@@ -15,13 +16,15 @@ type PdfRendererProps = {
 export const PdfRenderer = ({ url }: PdfRendererProps) => {
   const { toast } = useToast();
 
+  const { width, ref } = useResizeDetector();
+
   return (
     <div className="w-full bg-white rounded-md shadow flex flex-col items-center">
       <div className="h-14 w-full border-b border-zinc-200 flex justify-between items-center px-2">
         <div className="flex items-center gap-1.5">top bar</div>
       </div>
       <div className="flex-1 w-full max-h-screen">
-        <div className="">
+        <div ref={ref} className="">
           <Document
             loading={
               <div className="flex justify-center">
@@ -38,7 +41,7 @@ export const PdfRenderer = ({ url }: PdfRendererProps) => {
             file={url}
             className="max-h-full"
           >
-            <Page pageNumber={1} />
+            <Page width={width ? width : 1} pageNumber={1} />
           </Document>
         </div>
       </div>
