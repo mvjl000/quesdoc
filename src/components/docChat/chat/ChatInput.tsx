edad43d896b7100a096e7cf85db3e1207f84debf
Miłosz <milosz.devmail@gmail.com>
2023-10-04 18:23:37 +0200

    Handle loading, file processing and failed statuses

---
+++ src/components/docChat/chat/ChatInput.tsx
@@ -0,0 +1,11 @@
+type ChatInputProps = {
+  isDisabled?: boolean;
+};
+
+export const ChatInput = ({ isDisabled }: ChatInputProps) => {
+  return (
+    <div>
+      <p>ChatInput</p>
+    </div>
+  );
+};
--- src/components/docChat/chat/ChatWrapper.tsx
+++ src/components/docChat/chat/ChatWrapper.tsx
@@ -1,12 +1,93 @@
-import { Messages } from "@/components/docChat/chat/Messages";
+"use client";
 
-export const ChatWrapper = () => {
+import { Messages } from "@/components/docChat/chat/Messages";
+import { ChatInput } from "./ChatInput";
+import { trpc } from "@/app/_trpc/client";
+import { ChevronLeft, Loader2, XCircle } from "lucide-react";
+import Link from "next/link";
+import { buttonVariants } from "@/components/ui/button";
+
+type ChatWrapperProps = {
+  fileId: string;
+};
+
+export const ChatWrapper = ({ fileId }: ChatWrapperProps) => {
+  const { data, isLoading } = trpc.getFileUploadStatus.useQuery(
+    { fileId },
+    {
+      refetchInterval: (data) =>
+        data?.status === "SUCCESS" || data?.status === "FAILED" ? false : 500,
+    }
+  );
+
+  if (isLoading)
+    return (
+      <div className="relative min-h-full bg-zinc-50 flex divide-y divide-zinc-200 flex-col justify-between gap-2">
+        <div className="flex-1 flex justify-center items-center flex-col mb-28">
+          <div className="flex flex-col items-center gap-2">
+            <Loader2 className="h-8 w-8 text-primary animate-spin" />
+            <p className="font-semibold text-xl">Loading...</p>
+            <p className="text-zinc-500 text-sm">
+              We&apos;re preparing your PDF.
+            </p>
+          </div>
+        </div>
+
+        <ChatInput isDisabled={true} />
+      </div>
+    );
+
+  if (data?.status === "PROCESSING")
+    return (
+      <div className="relative min-h-full bg-zinc-50 flex divide-y divide-zinc-200 flex-col justify-between gap-2">
+        <div className="flex-1 flex justify-center items-center flex-col mb-28">
+          <div className="flex flex-col items-center gap-2">
+            <Loader2 className="h-8 w-8 text-primary animate-spin" />
+            <p className="font-semibold text-xl">Processing the document...</p>
+            <p className="text-zinc-500 text-sm">This won&apos;t take long.</p>
+          </div>
+        </div>
+
+        <ChatInput isDisabled={true} />
+      </div>
+    );
+
+  if (data?.status === "FAILED")
+    return (
+      <div className="relative min-h-full bg-zinc-50 flex divide-y divide-zinc-200 flex-col justify-between gap-2">
+        <div className="flex-1 flex justify-center items-center flex-col mb-28">
+          <div className="flex flex-col items-center gap-2">
+            <XCircle className="h-8 w-8 text-red-500" />
+            <p className="font-semibold text-xl">
+              The document is too heavy...
+            </p>
+            <p className="text-zinc-500 text-sm">
+              Your <span className="font-medium">Free</span> plan supports
+              documents only up to 4MB.
+            </p>
+            <Link
+              href="/dashboard"
+              className={buttonVariants({
+                variant: "secondary",
+                className: "mt-4",
+              })}
+            >
+              <ChevronLeft className="w-3 h-3 mr-1.5" />
+              Back to home
+            </Link>
+          </div>
+        </div>
+
+        <ChatInput isDisabled={true} />
+      </div>
+    );
+
   return (
     <div className="relative min-h-full bg-zinc-50 flex divide-y divide-zinc-200 flex-col justify-between gap-2">
       <div className="flex-1 justify-between flex flex-col mb-28">
         <Messages />
       </div>
-      {/* <ChatInput /> */}
+      <ChatInput />
     </div>
   );
 };
