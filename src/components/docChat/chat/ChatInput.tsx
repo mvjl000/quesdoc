0f187439d36f710a52ed26261f3401acc5fefccd
Miłosz <milosz.devmail@gmail.com>
2023-10-04 18:36:09 +0200

    Create chatInput component

--- src/components/docChat/chat/ChatInput.tsx
+++ src/components/docChat/chat/ChatInput.tsx
@@ -1,11 +1,35 @@
+import { Button } from "@/components/ui/button";
+import { Textarea } from "@/components/ui/textarea";
+import { SendIcon } from "lucide-react";
+
 type ChatInputProps = {
   isDisabled?: boolean;
 };
 
 export const ChatInput = ({ isDisabled }: ChatInputProps) => {
   return (
-    <div>
-      <p>ChatInput</p>
+    <div className="absolute bottom-0 left-0 w-full">
+      <form className="mx-2 flex flex-row gap-3 md:mx-4 md:last:mb-6 lg:mx-auto lg:max-w-2xl xl:max-w-3xl">
+        <div className="relative flex h-full flex-1 items-stretch md:flex-col">
+          <div className="relative flex flex-col w-full flex-grow p-4">
+            <div className="relative">
+              <Textarea
+                placeholder="Enter your question..."
+                rows={1}
+                maxRows={4}
+                autoFocus={true}
+                className="resize-none pr-12 text-base py-3 scrollbar-thumb-violet scrollbar-thumb-rounded scrollbar-track-violet-lighter scrollbar-w-2 scrolling-touchr"
+              />
+              <Button
+                aria-label="send message"
+                className="absolute bottom-[5px] right-[5px]"
+              >
+                <SendIcon className="w-4 h-4" />
+              </Button>
+            </div>
+          </div>
+        </div>
+      </form>
     </div>
   );
 };
