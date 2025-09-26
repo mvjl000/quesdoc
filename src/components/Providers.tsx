acdcd92ae3f71ba60d509c2cf495591350747b3e
Miłosz <milosz.devmail@gmail.com>
2025-09-26 23:47:08 +0200

    app url env var

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -6,20 +6,22 @@ import { trpc } from "@/app/_trpc/client";
 import { httpBatchLink } from "@trpc/client";
 
 export const Providers = ({ children }: { children: ReactNode }) => {
-  const [queryClient] = useState(() => new QueryClient());
-  const [trpcClient] = useState(() =>
-    trpc.createClient({
-      links: [
-        httpBatchLink({
-          url: "http://localhost:3000/api/trpc",
-        }),
-      ],
-    })
-  );
+    const [queryClient] = useState(() => new QueryClient());
+    const [trpcClient] = useState(() =>
+        trpc.createClient({
+            links: [
+                httpBatchLink({
+                    url: `${process.env.NEXT_PUBLIC_APP_URL}/api/trpc`,
+                }),
+            ],
+        })
+    );
 
-  return (
-    <trpc.Provider client={trpcClient} queryClient={queryClient}>
-      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-    </trpc.Provider>
-  );
+    return (
+        <trpc.Provider client={trpcClient} queryClient={queryClient}>
+            <QueryClientProvider client={queryClient}>
+                {children}
+            </QueryClientProvider>
+        </trpc.Provider>
+    );
 };
